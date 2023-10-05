cd14f4ff7448e18778cdc718317e764a13d70b48
Vipul Gupta <mail.vipulg@gmail.com>
2023-10-05 21:03:42 +0530

    Added CSS & Handle local Storage part as well as some ui modifications

--- App.js
+++ App.js
@@ -14,8 +14,8 @@ let page = 1;
 prevBtn.disabled = true;
 prevBtn.classList.add("disabled");
 currBtn.innerHTML = `current Page: ${page}`;
-let favoriteMovieStorage = [];
-// console.log(searchInput[0]);
+
+// console.log(hearts);
 
 
 // Task 1 : Display a list of Movies through an external API
@@ -29,9 +29,9 @@ async function fetchMovies(page) {
 fetchMovies(page);
 
 function displayMovies(allMoviesData) {
-    // console.log(allMoviesData);
+    console.log(allMoviesData);
     preloadedHtml.style.display = "none";
-    allMoviesData.map(({poster_path, title, vote_average, vote_count, overview, release_date})=>{
+    allMoviesData.map(({id, poster_path, title, vote_average, vote_count, overview, release_date})=>{
         let poster_image = `https://image.tmdb.org/t/p/original/${poster_path}`;
         if(poster_path == null) {
             poster_image = 'https://media.istockphoto.com/id/1271522601/photo/pop-corn-and-on-red-armchair-cinema.jpg?s=612x612&w=0&k=20&c=XwQxmfrHb-OwV5onPUW5ApB4RaGBK7poSIzZj4q_N_g=';
@@ -39,11 +39,11 @@ function displayMovies(allMoviesData) {
         const cardTemplate = 
         `<div class="movie-card">
             <img class="movie-image" src="${poster_image}" alt="${title}">
-            <i class="fa fa-heart-o" style="font-size:24px;color:var(--secondary-color)"></i>
-            <div class="movie-info-overlay">
-                <h3 class="movie-title">${title} <span class="releaseyear">(${release_date.split("-")[0]})</span></h3>
-                <div class="movie-vote-average">Rating: ${vote_average}</div>
-                <div class="movie-vote-count">Votes: ${vote_count}</div>
+            <i class="fa fa-heart-o" id="${id}" style="font-size:24px;color:var(--secondary-color)"></i>
+            <div class="movie-info-overlay" >
+                <h3 class="movie-title">${title}</h3>
+                <div class="movie-vote-average">Rating: <span class="rating">${vote_average}</span></div>
+                <div class="movie-vote-count">Votes: <span class="vote">${vote_count}</span></div>
                 <div class="movie-overview">
                     <h4>Overview</h4>
                     <div class="overview">${overview}</div>
@@ -54,6 +54,9 @@ function displayMovies(allMoviesData) {
         allMoviesDisplay.innerHTML += cardTemplate;
     });
 }
+// <span class="releaseyear">(${release_date.split("-")[0]})</span>
+
+
 
 // Task 2 : Sorting movies by their rating and by their release date
 sortByDateBtn.addEventListener('click', handleSortByDateBtn);
@@ -106,12 +109,13 @@ async function handleSearchBtnClick() {
     }
     else {
         allMoviesDisplay.innerHTML = "";
+        nextBtn.disabled = true;
+        nextBtn.classList.add("disabled");
         const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchMovie}&api_key=f531333d637d0c44abc85b3e74db2186&language=en-US`);
         const data = await res.json();
-        
+        allMovieData = data.results;
         if(data.results.length == 0) {
-            allMoviesDisplay.innerHTML = `<img src="./images/no-data-found.png" alt="no data found" />`;
-            return;
+            allMoviesDisplay.innerHTML = `<img class="temp" src="./images/no-data-found.png" alt="no data found" />`;
         }
         displayMovies(data.results);
         searchInput[0].value = "";
@@ -120,19 +124,80 @@ async function handleSearchBtnClick() {
 }
 
 // Task 4: Add and Remove Movies from Favorite
+function getFavoriteMoviesFromLS() {
+    let favoriteMovies;
 
+    if(localStorage.getItem('favorite') === null) {
+        favoriteMovies = [];
+    }
+    else {
+        favoriteMovies = JSON.parse(localStorage.getItem('favorite'));
+    }
+
+    return favoriteMovies;
+}
+allMoviesDisplay.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('fa') & e.target.classList.contains('fa-heart-o')) {
+        e.target.classList.remove('fa-heart-o');
+        e.target.classList.add('fa-heart');
+        e.target.dataset.mark = 'added-to-fav';
+        addToLocalStorage(e.target.parentElement);
+    }
+    else if(e.target.classList.contains('fa') & e.target.classList.contains('fa-heart')) {
+        e.target.classList.remove('fa-heart');
+        e.target.classList.add('fa-heart-o');
+        delete e.target.dataset.mark;
+        removeFromLS(e.target.id);
+    }
+})
+function addToLocalStorage(movieCard) {
+    let favMovie = {
+        id: movieCard.querySelector('i').id,
+        title: movieCard.querySelector('.movie-title').textContent,
+        poster_path: movieCard.querySelector('img').src.slice(36),
+        vote_count: movieCard.querySelector('.vote').textContent,
+        vote_average: movieCard.querySelector('.rating').textContent,
+        overview: movieCard.querySelector('.overview').textContent
+        // release_date: movieCard.querySelector('releaseyear').textContent
+    };
+    let favoriteMovie = getFavoriteMoviesFromLS();
+    favoriteMovie.push(favMovie);
+    localStorage.setItem('favorite', JSON.stringify(favoriteMovie));
+}
+
+function removeFromLS(id) {
+    let favoriteMovie = getFavoriteMoviesFromLS();
+    favoriteMovie.forEach((element, index) => {
+        if(element.id === id) {
+            favoriteMovie.splice(index, 1);
+        }
+    });
+    localStorage.setItem('favorite', JSON.stringify(favoriteMovie));
+}
 // Task 5: Implement the Favorites tab
 favoriteMovieTab.addEventListener('click', handleFavoriteMovieTabBtn);
 function handleFavoriteMovieTabBtn() {
+    let favoriteMoviesInLS = getFavoriteMoviesFromLS();
     allMoviesDisplay.innerHTML = "";
-    if(favoriteMovieStorage == 0) {
+    favoriteMovieTab.classList.add('active');
+    allMovieTab.classList.remove('active');
+    if(favoriteMoviesInLS.length === 0) {
+        allMoviesDisplay.innerHTML = `<img class="temp" src="./images/no-data-found.png" alt="no data found" />`;
         currBtn.innerHTML = `current Page: 1`;
+    }
+    else {
+        // console.log(favoriteMoviesInLS);
+        displayMovies(favoriteMoviesInLS);
     }
 
 }
 allMovieTab.addEventListener('click', handleAllMovieTabBtn);
 function handleAllMovieTabBtn() {
     allMoviesDisplay.innerHTML = "";
+    favoriteMovieTab.classList.remove('active');
+    allMovieTab.classList.add('active');
+    nextBtn.disabled = false;
+    nextBtn.classList.remove('disabled');
     fetchMovies(page);
     currBtn.innerHTML = `current Page: ${page}`;
 }
